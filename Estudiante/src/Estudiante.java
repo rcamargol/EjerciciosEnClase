0b933fb987a01e7c9f5698b88b06e0b139e9935a
rcamargol <rcamargol@unbosque.edu.co>
2022-08-21 09:46:06 -0500

    commit inicial

---
+++ Estudiante/src/Estudiante.java
@@ -0,0 +1,21 @@
+
+public class Estudiante {
+	int nota;
+	String nombre;
+	String password;
+	
+	public int solicitarNota() {
+		return 0;
+	}
+	
+	public int calcularPromedio() {
+		return 0;
+	}
+	
+	public String mostrarResultados() {
+		return "";
+	}
+	
+	//Aqui van los getters y setters
+	
+}
